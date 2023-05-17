c7c8bc8e36b3d51c5e2afa0ff50ee0f12ffcd88b
Andy <andycao@yeah.net>
2023-05-18 08:57:07 +1200

    feat: add new modifier combination

--- src/holistic.ts
+++ src/holistic.ts
@@ -1,24 +1,36 @@
 import { map, mapDoubleTap, mapSimultaneous, rule } from 'karabiner.ts'
 
 export const holistic = rule('Global shortcut keys').manipulators([
-  // Remap caps_lock to esc
+  // Remap to esc
   map('⇪').toHyper().toIfAlone('⎋'),
 
+  // Double tap modifiers
+  // Left hand
   mapSimultaneous(['f', 'd']).to('left_command'),
   mapSimultaneous(['d', 's']).to('left_option'),
   mapSimultaneous(['s', 'a']).to('left_control'),
-  mapSimultaneous(['a', 'f']).to('left_shift'),
-  mapSimultaneous(['s', 'f']).to('left_shift', 'left_option'),
+  mapSimultaneous(['d', 'g']).to('left_shift'),
 
+  mapSimultaneous(['a', 'd']).to('left_shift', 'left_control'),
+  mapSimultaneous(['s', 'f']).to('left_shift', 'left_option'),
+  mapSimultaneous(['a', 'f']).to('left_shift', 'left_command'),
+  mapSimultaneous(['f', 'c']).to('left_command', 'left_option'),
+  // Right hand
   mapSimultaneous(['j', 'k']).to('right_command'),
   mapSimultaneous(['k', 'l']).to('right_option'),
   mapSimultaneous(['l', 'semicolon']).to('right_control'),
-  mapSimultaneous(['j', 'semicolon']).to('right_shift'),
+  mapSimultaneous(['k', 'h']).to('right_shift'),
+  
+  mapSimultaneous(['k', 'semicolon']).to('right_shift', 'right_control'),
   mapSimultaneous(['j', 'l']).to('right_shift', 'right_option'),
+  mapSimultaneous(['j', 'semicolon']).to('right_shift', 'right_command'),
 
-  // Single tap right shift to read selected text
-  map('right_shift')
-    .to('right_shift')
+
+  // Read selected text
+  // left_command key will be accidentally tapped
+  // left_control key has been used for open below side bar
+  map('left_option')
+    .to('left_option')
     .toIfAlone('p', ['left_option', 'left_control']),
 
   // Double click esc to look up in dictionary
